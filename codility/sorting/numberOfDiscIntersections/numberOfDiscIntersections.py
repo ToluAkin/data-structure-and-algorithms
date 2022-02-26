7aa07a99353dece106ebdd1252a14a5b9c3662d7
Toluwalase <toluakintoye@gmail.com>
2022-02-26 22:05:15 +0100

    solved codility codility/sorting/numberOfDiscIntersections

--- codility/sorting/numberOfDiscIntersections/numberOfDiscIntersections.py
+++ codility/sorting/numberOfDiscIntersections/numberOfDiscIntersections.py
@@ -2,8 +2,10 @@ def solution(A):
     lenA = len(A)
     leftValues = []
     rightValues = []
-    count = 0
     index = 0
+    open = 0
+    intersect = 0
+    position = 0
             
     for i in range(lenA):
         leftValues.append(i - A[i])
@@ -11,14 +13,19 @@ def solution(A):
 
     leftValues.sort()
     rightValues.sort()
-    
-    for i in range(lenA):
-        while (index < lenA and leftValues[index] <= rightValues[i]):
-            count += (index - i)
-            index += 1
 
-            if count > 10000000:
-                return -1
-    print(count)
-    
-solution([1, 5, 2, 1, 4, 0])
+    while position < len(leftValues):
+        if leftValues[position] <= rightValues[index]:
+            intersect += open
+            open += 1
+            position += 1
+        else:
+            open -= 1
+            index += 1
+        
+        if intersect > 10000000:
+            return -1
+    return intersect
+# print(solution([1, 5, 2, 1, 4, 0]))
+print(solution([1, 5, 8, 7, 8, 4, 8, 5, 0, 5]))
+print(solution([1, 0, 1, 0, 1]))
