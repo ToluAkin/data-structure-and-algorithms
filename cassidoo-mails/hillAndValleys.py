8a13cde30f21f9769f56954e9062d096a6002d12
Toluwalase Akintoye <toluakintoye@gmail.com>
2024-03-23 11:05:00 +0000

    update fix

--- cassidoo-mails/hillAndValleys.py
+++ cassidoo-mails/hillAndValleys.py
@@ -5,10 +5,17 @@
 def hills(arr: list) -> int:
     pos = 1
     hill = 0
+    left = 0
 
     while pos < len(arr) - 1:
-        if arr[pos - 1] < arr[pos] > arr[pos + 1]:
+        if arr[pos] == arr[pos + 1] and left != arr[pos]:
+            left = arr[pos - 1]
+        elif arr[pos] != arr[pos + 1]:
+            left = 0
+
+        if arr[pos - 1] < arr[pos] > arr[pos + 1] or left < arr[pos - 1] == arr[pos] > arr[pos + 1]:
             hill += 1
+        
         pos += 1
 
     return hill
@@ -16,9 +23,16 @@ def hills(arr: list) -> int:
 def valleys(arr: list) -> int:
     pos = 1
     valley = 0
+    left = 0
 
     while pos < len(arr) - 1:
-        if arr[pos - 1] > arr[pos] < arr[pos + 1] > arr[pos] or arr[pos - 1] == arr[pos] < arr[pos + 1]:
+        if arr[pos] == arr[pos + 1] and left != arr[pos]:
+            left = arr[pos - 1]
+
+        elif arr[pos] != arr[pos + 1]:
+            left = 0
+
+        if arr[pos - 1] > arr[pos] < arr[pos + 1] or left > arr[pos - 1] == arr[pos] < arr[pos + 1]:
             valley += 1    
         pos += 1
 
@@ -29,14 +43,18 @@ def valleys(arr: list) -> int:
 # print(hills(arr)) # returns 1
 # print(valleys(arr)) # returns 0
 
-arr = [1,0,1]
-print(hills(arr)) # returns 0
-print(valleys(arr)) # returns 1
+# arr = [1,0,1]
+# print(hills(arr)) # returns 0
+# print(valleys(arr)) # returns 1
 
-arr = [7,6,5,5,4,1]
-print(hills(arr)) # returns 0
-print(valleys(arr)) # returns 0
+# arr = [7,6,5,5,4,1]
+# print(hills(arr)) # returns 0
+# print(valleys(arr)) # returns 0
 
 # arr = [3,4,1,1,6,5]
 # print(hills(arr)) # returns 2
 # print(valleys(arr)) # returns 1
+
+arr = [1,4,5,5,4,6,7]
+print(hills(arr)) # returns 1
+print(valleys(arr)) # returns 1
